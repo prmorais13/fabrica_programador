9954a61ed85ef211b6c5f5a4aa04d195ad090c1a
prmorais13 <prmorais_13@hotmail.com>
2017-10-19 01:23:29 -0200

    Aula 3 - Partes de 4 a 9 - Casa

--- curso_java_web1/src/main/webapp/js/autenticacao.js
+++ curso_java_web1/src/main/webapp/js/autenticacao.js
@@ -1,18 +1,19 @@
 // Declaração da classe/objeto
-Autenticacao = function () { }
-
-// Declaração do método
-Autenticacao.prototype.validarCampos = function () {
-    usuario = document.getElementById('txtUsuario').value;
-    senha = document.getElementById('txtSenha').value;
-
-    if (usuario == "" || senha == "") {
-        window.alert('Usuário e Senha devem ser informados!');
-        return false;
+Autenticacao = function () {
+    
+    // Declaração do método
+    this.validarCampos = function () {
+        usuario = document.getElementById('txtUsuario').value;
+        senha = document.getElementById('txtSenha').value;
+        
+        if (usuario == "" || senha == "") {
+            window.alert('Usuário e Senha devem ser informados!');
+            return false;
+        }
+        
+        return true;
     }
-
-    return true;
 }
-
+    
 // Estanciando o objeto
 aut = new Autenticacao();
